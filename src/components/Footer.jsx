fa2e01afb269aa090901a2f17f82b08558998f6f
Aly Alsamouly <alyalsamouly@MacBook-Pro.local>
2026-05-19 20:46:10 -0700

    Remove contact info from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,6 @@ const Footer = () => {
       <div className="container text-center">
         <h2 className="footer-logo script-font">Abbey & Aly</h2>
         <p className="footer-message">We can't wait to celebrate with you in Cairo!</p>
-        <p className="footer-contact">Please reach out to Abbey or Aly if you have any questions.</p>
         <p className="footer-date">May 3, 2027</p>
       </div>
     </footer>
